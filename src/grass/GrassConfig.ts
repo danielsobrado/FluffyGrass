42d35d419818b2117f9d0da1431f4b87f7908aa1
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-08-02 04:41:09 +0400

    feat: define procedural grass configuration

--- src/grass/GrassConfig.ts
+++ src/grass/GrassConfig.ts
@@ -4,13 +4,55 @@ export interface GrassLodConfig {
   farMaxDistance: number;
 }
 
+export interface GrassGeometryConfig {
+  variantCount: number;
+  bladesPerClump: number;
+  bladeSegments: number;
+  clumpRadius: number;
+  bladeHeightMin: number;
+  bladeHeightMax: number;
+  bladeWidthMin: number;
+  bladeWidthMax: number;
+  bladeLeanMin: number;
+  bladeLeanMax: number;
+}
+
+export interface GrassDistributionConfig {
+  seed: number;
+  rootSink: number;
+  maxSlopeDegrees: number;
+  heightVariation: number;
+  widthVariation: number;
+  densityMin: number;
+  densityMax: number;
+  densityScale: number;
+}
+
+export interface GrassWindConfig {
+  directionX: number;
+  directionZ: number;
+  strength: number;
+  gustScale: number;
+  gustSpeed: number;
+  flutterStrength: number;
+  flutterSpeed: number;
+}
+
+export interface GrassMaterialConfig {
+  baseColor: string;
+  tipColor: string;
+  dryColor: string;
+  rootDarkening: number;
+  normalUp: number;
+  ambientBoost: number;
+}
+
 export interface GrassConfig {
-  modelPath: string;
-  geometryName: string;
   instanceCount: number;
-  geometryScale: number;
   patchSize: number;
-  alphaTexturePath: string;
-  noiseTexturePath: string;
+  geometry: GrassGeometryConfig;
+  distribution: GrassDistributionConfig;
+  wind: GrassWindConfig;
+  material: GrassMaterialConfig;
   lod: GrassLodConfig;
 }
